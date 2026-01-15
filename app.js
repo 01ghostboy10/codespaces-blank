9fe7f1bf30f3da7d08d002456add19dc29d2e250
01ghostboy10 <levana.rai.simcik@mensa.cz>
2026-01-15 13:57:23 +0000

    colour changes

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
+//popup code
 const popup = document.querySelector('.full-screen');
 console.log(popup);
 
 function togglePopup() {
     popup.classList.toggle('hidden');
 }
+
+
+//return the name!
+let input = localStorage.getItem('input');
+
+function returnText() {
+  input = document.getElementById("userInput").value.toUpperCase();
+  localStorage.setItem('input', input);
+  document.getElementById("dialogueBox").textContent = "Welcome, " + input;
+}
+
+// print ti
+if (input !== null) {
+  document.getElementById("dialogueBox").textContent = input;
+}
+
+
+window.onload = function () {
+    function togglePopup() {
+        popup.classList.false('hidden');
+    }
+};
